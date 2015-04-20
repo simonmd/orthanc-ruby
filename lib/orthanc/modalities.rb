1b010a53cc0feaea2be638b41531c1e1c673f1e1
Simon Rascovsky <simonmd@gmail.com>
2015-04-19 19:25:12 -0500

    handle exceptions in modality DICOM operations

--- lib/orthanc/modalities.rb
+++ lib/orthanc/modalities.rb
@@ -24,33 +24,69 @@ module Orthanc
     end
 
     # POST /modalities/{dicom}/echo
-    def echo(payload = {}) # C-Echo SCU
-      handle_response(base_uri["echo"].post(payload))
+    def echo(payload = {}) # C-Echo SCU. Return true if successful
+      base_uri["echo"].post(payload){|response, request, result, &block|
+        if response.code == 200
+          return true
+        else
+          return false
+        end
+      }
     end
 
     # POST /modalities/{dicom}/find
-    def find(payload = {})
-      handle_response(base_uri["find"].post(payload))
+    def find(payload = {}) # C-Echo SCU. Return true if successful
+      rsp = base_uri["find"].post(payload){|response, request, result, &block|
+        if response.code == 200
+          return rsp
+        else
+          return false
+        end
+      }
     end
 
     # POST /modalities/{dicom}/find-patient
-    def find_patient(payload = {})
-      handle_response(base_uri["find-patient"].post(payload))
+    def find_patient(payload = {}) # eg. '{"PatientName":"JOD*","PatientSex":"M"}'
+      rsp = base_uri["find-patient"].post(payload){|response, request, result, &block|
+        if response.code == 200
+          return rsp
+        else
+          return false
+        end
+      }
     end
 
     # POST /modalities/{dicom}/find-series
-    def find_series(payload = {})
-      handle_response(base_uri["find-series"].post(payload))
+    def find_series(payload = {}) # eg. '{"PatientID":"0555643F"}'
+      rsp = base_uri["find-series"].post(payload){|response, request, result, &block|
+        if response.code == 200
+          return rsp
+        else
+          return false
+        end
+      }
     end
 
     # POST /modalities/{dicom}/find-study
-    def find_study(payload = {})
-      handle_response(base_uri["find-study"].post(payload))
+    def find_study(payload = {}) # eg. '{"PatientID":"0555643F","StudyInstanceUID":"1.2.840.113704.1.111.2768.1239195678.57"}'
+      rsp = base_uri["find-study"].post(payload){|response, request, result, &block|
+        if response.code == 200
+          return rsp
+        else
+          return false
+        end
+      }
     end
 
     # POST /modalities/{dicom}/store
     def store(payload = {}) # POST body = UUID series, UUID instance, or raw DICOM file
-      handle_response(base_uri["store"].post(payload))
+      base_uri["store"].post(payload){|response, request, result, &block|
+        if response.code == 200
+          return true
+        else
+          return false
+        end
+      }
     end
 
   end
